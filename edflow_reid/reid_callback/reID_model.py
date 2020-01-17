e174361558c9cd24396e8ad64e31a728ad918f67
mritv <mritvik@ymail.com>
2020-01-17 18:00:49 +0100

    The reID model

---
+++ edflow_reid/reid_callback/reID_model.py
@@ -0,0 +1,66 @@
+import os
+from importlib import import_module
+
+import tensorflow as tf
+
+TRIP_CHECK = os.environ.get("REID_CHECKPOINT", "checkpoint/checkpoint-25000")
+
+TRIP_W = 256
+TRIP_H = 256
+
+
+class reIdModel(object):
+    """ReID model as from the paper "In defense of the triplet loss\""""
+    @property
+    def name(self):
+        return "triplet_reid"
+
+    def __init__(
+        self,
+        model_name="resnet_v1_50",
+        head_name="fc1024",
+        w=TRIP_W,
+        h=TRIP_H,
+        nc=3,
+        edim=128,
+        is_train=False,
+    ):
+        """Args:
+            model_name (str): Which base model to use.
+            head_name (str)L Which output head to use.
+            w (int): Image width.
+            h (int): Image height.
+            nc (int): Image channels.A
+            edim (int): Embedding dimension.
+            is_train (bool): Is it training or not?
+        """
+        model = import_module(
+            "edflow_reid.triplet_reid.triplet_reid.nets." + model_name
+        )
+        head = import_module("edflow_reid.triplet_reid.triplet_reid.heads." + head_name)
+
+        self.model_name = "triplet_reid"
+
+        with tf.variable_scope(self.model_name, reuse=tf.AUTO_REUSE):
+            self.images = tf.placeholder(tf.float32, shape=[None, h, w, nc])
+            input_images = self.images
+            endpoints, body_prefix = model.endpoints(
+                input_images, is_training=is_train, prefix=self.model_name + "/"
+            )
+            with tf.name_scope("head"):
+                endpoints = head.head(endpoints, edim, is_training=is_train)
+
+        self.embeddings = endpoints
+
+        globs = tf.global_variables()
+        self.variables = [v for v in globs if self.model_name in v.name]
+
+    @property
+    def inputs(self):
+        # (bs, h, w, c) in [-1, 1]
+        return {"image": self.images}
+
+    @property
+    def outputs(self):
+        # (bs, 128
+        return {"embeddings": self.embeddings}
